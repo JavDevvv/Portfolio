094ea021a9205a4b8decd5aa2063b8d69a68c371
JavDevvv <xjkeme@icloud.com>
2026-01-07 12:38:50 -0600

    cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1gKhtWFUa7zm1gUxQmXkfA3Z2Kxgi5oI7/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ymPmTye-j1VPC6w-RmoXTxChPIcnnx4m/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
